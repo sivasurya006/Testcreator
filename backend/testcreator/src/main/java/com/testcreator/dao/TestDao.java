48004f27b63c41a697f90cb02cfb97a9cafedd22
pirthikacd <cpirthika5760@gmail.com>
2026-02-02 09:44:55 +0530

    Dashboard

---
+++ backend/testcreator/src/main/java/com/testcreator/dao/TestDao.java
@@ -0,0 +1,18 @@
+package com.testcreator.dao;
+
+import java.sql.Connection;
+
+public class TestDao {
+	
+	
+	private Connection connection;
+
+	public TestDao(Connection connection) {
+		this.connection = connection;
+	}
+	public List<TestDto>  getAllTests(int classroomId){
+		List<TestDto>
+	}
+	
+
+}
