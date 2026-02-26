019b196b89294fb8843d6fcb9e450b14b3371dfa
sivasurya006 <titansurya88@gmail.com>
2026-02-26 17:32:55 +0530

    filter changed

--- backend/testcreator/src/main/java/com/testcreator/filters/CorsFilter.java
+++ backend/testcreator/src/main/java/com/testcreator/filters/CorsFilter.java
@@ -1,52 +1,41 @@
 package com.testcreator.filters;
 
 import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpFilter;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CorsFilter extends HttpFilter implements Filter {
+public class CorsFilter implements Filter {
 
-	public CorsFilter() {
-		super();
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("CORS Filter initialized");
+    }
 
-	public void destroy() {
+    @Override
+    public void destroy() {
+    }
 
-	}
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
 
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
 
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
+        // Allow only your frontend origin
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Content-Type, Authorization, X-Client-Type, X-ClassroomId, X-TestId, X-QuestionId, X-AttemptId");
 
-		String origin = request.getHeader("Origin");
+        // Handle preflight request
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
 
-		response.setHeader("Access-Control-Allow-Origin", origin);
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-
-		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-		response.setHeader("Access-Control-Allow-Headers",
-				"Content-Type, Authorization, X-Client-Type, X-ClassroomId, X-TestId, X-QuestionId, X-AttemptId");
-
-		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
-			return;
-		}
-
-		chain.doFilter(req, res);
-	}
-
-	public void init(FilterConfig fConfig) throws ServletException {
-		System.out.println("inited");
-	}
-
+        chain.doFilter(req, res);
+    }
 }
